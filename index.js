f7efb17e45f7adf5a083197fc01dc9ceb930fe76
Darkowl999 <raulalbertoortiz290199@hotmail.com>
2021-02-24 03:19:32 -0400

    añadiendo nodemon

---
+++ index.js
@@ -0,0 +1,9 @@
+const http= require('http');
+const server= http.createServer((req,res)=>{
+    res.status=200;
+    res.setHeader('Content-Type','text/plain');
+    res.end('Hello world');
+});
+server.listen(3000,()=>{
+    console.log("Serve on port 3000");
+});
